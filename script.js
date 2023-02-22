8e39ddc0b26c184766cd8939b277ce08c32ec24f
Chyniece Matthews <cfmatthews2@gmail.com>
2023-02-22 16:08:21 -0600

    New guessing game

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-/*Your JS file must contain a 'use strict' declaration at the top, and your javascript file must be linked in your html file as an external script file.
-
-Make sure that all code is cleanly written with correct indentation and syntax. Look at an example JavaScript Style Guide. Note that your JS must pass the rules in the linter that you installed.
-
-Follow the Add, Commit, Push process. It should be evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit.*/
 "use strict";
-
+//display user name 
 let uname = prompt("Enter your name here")
 
 const x = document.querySelector("#hello");
@@ -19,7 +14,86 @@ function displayName(username){
 
 displayName(uname) 
 
- 
+//Guess Game
+
+ let randomNumber = Math.floor(Math.random()*50) + 1;
+ const guesses = document.querySelector(".guesses");
+ const lastResult = document.querySelector(".lastResult");
+ const lowHi = document.querySelector(".lowHi");
+
+ const guessSubmit = document.querySelector(".guessSubmit");
+ const guessField = document.querySelector(".guessField");
+
+ let guessCount = 1;
+ let resetButton;
+ function checkGuess(){
+    const userGuess = Number(guessField.value);
+    if(guessCount === 1 ) {
+        guesses.textContent = "Previous guesses: ";
+    }
+    guesses.textContent += `${userGuess}`;
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "You got it!";
+        
+        lowHi.textContent = "";
+        setGameOver();
+    } else if (guessCount === 6) {
+        lastResult.textContent = "Whomp Whomp Whomp, you lost.";
+        lowHi.textContent = "";
+        setGameOver()
+    } else {
+        lastResult.textContent = "Nope!";
+        if (userGuess< randomNumber) {
+            lowHi.textContent = "Yo last guess was too dang low";
+        } else if (userGuess> randomNumber) {
+            lowHi.textContent = "Yo last guess was too dang High";
+        }
+        
+    }
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
+
+ }
+
+ checkGuess(){
+    guessSubmit.addEventListener("click",checkGuess);
+ }
+
+ function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Play Again";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+ }
+
+function resetgame(){
+    guessCount = 1;
+    
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for (const resetPara of resetParas) {
+        resetPara.textContent = "";
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    randomNumber = Math.floor(Math.random() * 50) + 1;
+}
+
+
+
+
+
+//Mini game about me
+
 let score = 0
 function quizGame() {
    
