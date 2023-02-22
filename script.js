e1a369f5fc80c6e9279b4fedb2e2d9e89f955b56
Chyniece Matthews <cfmatthews2@gmail.com>
2023-02-21 23:25:07 -0600

    edits

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 Make sure that all code is cleanly written with correct indentation and syntax. Look at an example JavaScript Style Guide. Note that your JS must pass the rules in the linter that you installed.
 
 Follow the Add, Commit, Push process. It should be evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit.*/
+"use strict";
 
 let name = prompt("Enter your name here")
 
 const x = document.querySelector("#hello");
 
 function displayName(username){
-    if(username === null || username.length== 0){
+    if(username === null || username.length == 0){
         x.innerHTML = "Hello";   
     } else{
         x.innerHTML =  `Hello ${username}`;
@@ -17,7 +18,9 @@ function displayName(username){
 }
 
 displayName(name) 
-let score = 0
+
+
+
 function quizGame() {
    
     let school = prompt("Did I go to Marymount for undergrad?")
@@ -26,12 +29,44 @@ function quizGame() {
         
     }
     else{ 
-        alert("incorrect"); 
+        alert("Incorrect"); 
     }
-    let work = prompt("Did I work at COOP Careers?");
-    let hood = prompt("Did I live in Paris?");
-    let hobby = prompt("Am I interested in The Arts?");
+    let work = prompt("Did I work at COOP Careers?")
+    if (work.toLowerCase() == "yes" || work.toLowerCase() =="y") {
+        alert("Correct"), score++;
+        
+    }
+    else{ 
+        alert("Incorrect"); 
+    }
 
+    let hood = prompt("Did I live in Paris?")
+    if (hood.toLowerCase() == "no" || hood.toLowerCase() =="n") {
+        alert("Correct"), score++;
+        
+    }
+    else{ 
+        alert("Incorrect"); 
+    }
+    
+    let hobby = prompt("Am I interested in The Arts?")
+    if (hobby.toLowerCase() == "yes" || hobby.toLowerCase() =="y") {
+        alert("Correct"), score++;
+        
+    }
+    else{ 
+        alert("Incorrect"); 
+    }
+    let active = prompt("Do I like sports?")
+    if (active.toLowerCase() == "no" || active.toLowerCase() =="n") {
+        alert("Correct"), score++;
+        
+    }
+    else{ 
+        alert("Incorrect"); 
+    }
 }
-
+ function onSubmit(){
+    alert("Thank you" + name )
+ }
 
